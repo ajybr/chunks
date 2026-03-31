37b881eaa1da7adf8010026a6200ff402e22b2df
ajybr <ajaystd9756@gmail.com>
2026-03-31 13:54:59 +0530

    feat(update): sidebar is now part of layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,51 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
-import type { Metadata } from 'next'
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+	subsets: ["latin"],
+	variable: "--font-mono",
+});
 
 export const metadata: Metadata = {
-  title: 'chunks',
-  description: 'cloud service',
-}
+	title: "chunks",
+	description: "cloud service",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en"
-    suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
-    >
-      <body className={`${inter.variable} ${fontMono.variable} antialiased`}>
-        <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
-          <ThemeProvider>
-          {children}
-          </ThemeProvider>
-        </ClerkProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html
+			lang="en"
+			suppressHydrationWarning
+			className={cn(
+				"antialiased",
+				fontMono.variable,
+				"font-sans",
+				inter.variable,
+			)}
+		>
+			<body className={`${inter.variable} ${fontMono.variable} antialiased`}>
+				<ClerkProvider>
+					<ThemeProvider>
+						<SidebarProvider>
+							<AppSidebar variant="inset" />
+							<SidebarInset> {children}</SidebarInset>
+						</SidebarProvider>
+					</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-import { SidebarProvider } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/AppSidebar"
 import { StorageLayout } from "@/components/StorageLayout"
 import { getChildren } from "@/server/db/queries"
 
 export default async function Home() {
   const items = await getChildren(null, "System")
-  console.log(items)
 
   return (
-    <SidebarProvider>
-      <AppSidebar />
       <StorageLayout
         items={items as any}
         initialFolderId={null}
         breadcrumbPath={[{ id: null, name: "My Files" }]}
       />
-    </SidebarProvider>
   )
 }
